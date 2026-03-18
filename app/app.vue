f21bde6142365eec15f7e2375e6547109a351d64
Zak <tsienyanel@gmail.com>
2026-03-18 20:33:52 +0200

    setup fonts and modules

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="font-montserrat container mx-auto p-4">
+    <NuxtPage />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,43 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+/* // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true }
 })
+ */
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    resetAsyncDataToUndefined: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+  devtools: { enabled: true },
+  modules: [
+   "@nuxtjs/tailwindcss",
+   "@nuxtjs/google-fonts",
+   "@nuxt/icon",
+   "@nuxt/image"
+  ],
+
+  googleFonts: {
+    families: {
+      Montserrat: true,
+    },
+  },
+});
